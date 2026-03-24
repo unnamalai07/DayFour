116f1c963d2a6f35a2f555b37b6f32f9e6d4ef56
unnamalai07 <unnamalaisolai07@gmail.com>
2026-03-24 17:27:47 +0530

    Refactor matchcase function and remove unused Tasl2.py file

--- Task1.py
+++ Task1.py
@@ -47,3 +47,21 @@ def matchcase(inputId):
 if __name__ == "__main__":
     inputId = int(input("Enter the inputId "))
     print(matchcase(inputId))
+
+
+
+
+def matchcase(inputId):
+    try : 
+        result = None
+        match inputId:
+            case "a"|"o":
+                print("Vowels")
+            case _:
+                return "Logic is not implemented for this inputId"
+    except Exception as e:
+        return result 
+
+if __name__ == "__main__":
+    inputId = int(input("Enter the inputId "))
+    print(matchcase(inputId))
